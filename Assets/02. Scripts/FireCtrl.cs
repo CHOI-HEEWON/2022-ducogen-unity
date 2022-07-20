fda3e693120fa322b07b1cfdb44071e0e17c9d0d
CHOI-HEEWON <choihw.ceo@gmail.com>
2022-07-20 14:59:27 +0900

    commit

--- Assets/02. Scripts/FireCtrl.cs
+++ Assets/02. Scripts/FireCtrl.cs
@@ -7,14 +7,34 @@ public class FireCtrl : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePos;
 
+    // AudioSource 컴포넌트 저장할 변수
+    private AudioSource audio;
+
+    // 발생시킨 음원 파일을 저장할 변수
+    public AudioClip fireSfx;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // method for generating gameobject or prefab
-            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+            Fire();
         }
+    }
+
+    void Fire()
+    {
+        // 게임오브젝트 또는 프리팹 생성하는 메소드
+        // Instantiate(생성할객체, 위치, 각도)
+        Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+
+        // 총 발사 사운드 실행
+        audio.PlayOneShot(fireSfx, 1.0f);
     }
     
     /*
